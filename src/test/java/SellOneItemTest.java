d9ebfec68342daf012b1490fc5b46f4bdb80caeb
Marcin Gryszko <marcin@grysz.com>
2016-10-18 07:31:24 +0200

    Display total of one item with any barcode mapped to price

--- src/test/java/SellOneItemTest.java
+++ src/test/java/SellOneItemTest.java
@@ -3,26 +3,31 @@ import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Map;
+
 public class SellOneItemTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
     @Test
     public void displayTotalWhenPriceFound() {
+        String barcode = "12345";
+        pricesByBarcode = Collections.singletonMap(barcode, new BigDecimal(10));
         context.checking(new Expectations() {{
             oneOf(display).displayTotal("total: 10 €");
         }});
 
-        String barcode = "12345";
         itemScanned(barcode);
     }
 
+    private Map<String, BigDecimal> pricesByBarcode;
     private Display display = context.mock(Display.class);
 
     private void itemScanned(String barcode) {
-        if (barcode.equals("12345")) {
-            display.displayTotal("total: 10 €");
-        }
+        BigDecimal price = pricesByBarcode.get(barcode);
+        display.displayTotal(String.format("total: %s €", price));
     }
 
     public interface Display {
